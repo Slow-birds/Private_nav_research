795ddcec844e4d64c9c213e84c6aa7a357e06d90
Slow-birds <1782023512@qq.com>
2024-12-11 21:43:15 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,128 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import copy
+from pyecharts.charts import Line
+import pyecharts.options as opts
+from WindPy import w
+
+w.start()
+
+
+# nav_adjusted
+def get_nav_adjusted(nav_df):
+    nav_df_copy = nav_df.copy()
+    nav_df_copy["nav_adjusted"] = np.nan
+    nav_df_copy.loc[0, "nav_adjusted"] = 1
+    for i in range(1, len(nav_df_copy)):
+        nav_adjusted_new = (
+            nav_df_copy.loc[i, "nav_accumulated"]
+            - nav_df_copy.loc[i - 1, "nav_accumulated"]
+        ) / nav_df_copy.loc[i - 1, "nav_unit"] + 1
+        nav_adjusted_new *= nav_df_copy.loc[i - 1, "nav_adjusted"]
+        nav_df_copy.loc[i, "nav_adjusted"] = nav_adjusted_new
+    return nav_df_copy
+
+
+def infer_frequency(df_nav):
+    date = df_nav["date"].values
+    # 如果大部分日期间隔为 1 天，那么数据可能是日度的
+    if (np.diff(date) == np.timedelta64(1, "D")).mean() > 0.75:
+        return "D"
+    elif (np.diff(date) >= np.timedelta64(5, "D")).mean() > 0.75:
+        return "W"
+    else:
+        print("无法推断频率,自动转为周度")
+        return "W"
+
+
+def get_nav_lines(df, fund_name, benchmark_name):
+
+    x_data = df["date"].dt.strftime("%Y-%m-%d").tolist()
+    ys_data = (
+        (df.drop(columns=["date", "nav_unit", "nav_accumulated"], axis=1) - 1) * 100
+    ).round(2)
+    min_data = (ys_data.values.min() - 0.1*abs(ys_data.values.min())).round()
+    max_data = (ys_data.values.max() + 0.1*abs(ys_data.values.max())).round()
+    names = [
+        f"{fund_name}_累计收益(%)",
+        f"{benchmark_name}_累计收益(%)",
+        f"{fund_name}_超额收益(%)",
+    ]
+    init_opts = {
+        "width": "1500px",
+        "height": "500px",
+        "is_horizontal_center": True,
+    }
+    line = Line(init_opts)
+    line.add_xaxis(x_data)
+    for i, name in enumerate(names):
+        y_values = ys_data.iloc[:, i].tolist()
+        line.add_yaxis(name, y_values, symbol="none")
+
+    line.set_global_opts(
+        legend_opts=opts.LegendOpts(
+            textstyle_opts=opts.TextStyleOpts(font_weight="bold", font_size=20)
+        ),
+        datazoom_opts=[
+            opts.DataZoomOpts(range_start=0, range_end=100, orient="horizontal")
+        ],
+        title_opts=opts.TitleOpts("Value over Time"),
+        yaxis_opts=opts.AxisOpts(min_=min_data, max_=max_data),
+        tooltip_opts=opts.TooltipOpts(trigger="axis"),
+    ).set_series_opts(
+        linestyle_opts=opts.LineStyleOpts(width=2),
+    )
+    # line.render_notebook()
+    return line
+
+
+def get_drawdown_lines(df, fund_name, benchmark_name):
+    x_data = df["date"].dt.strftime("%Y-%m-%d").tolist()
+    ys_data = ((df.drop("date", axis=1)) * 100).round(2)
+
+    min_data = (ys_data.values.min() - 0.1*abs(ys_data.values.min())).round(0)
+    max_data = ys_data.values.max().round(0)
+    names = [
+        f"{fund_name}_回撤(%)",
+        f"{benchmark_name}_回撤(%)",
+        f"{fund_name}_超额回撤(%)",
+    ]
+    init_opts = {
+        "width": "1500px",
+        "height": "500px",
+        "is_horizontal_center": True,
+    }
+    line = Line(init_opts)
+    line.add_xaxis(x_data)
+    for i, name in enumerate(names):
+        y_values = ys_data.iloc[:, i].tolist()
+        line.add_yaxis(name, y_values, symbol="none")
+
+    line.set_global_opts(
+        legend_opts=opts.LegendOpts(
+            textstyle_opts=opts.TextStyleOpts(font_weight="bold", font_size=20)
+        ),
+        datazoom_opts=[
+            opts.DataZoomOpts(range_start=0, range_end=100, orient="horizontal")
+        ],
+        title_opts=opts.TitleOpts("Value over Time"),
+        yaxis_opts=opts.AxisOpts(min_=min_data, max_=max_data),
+        tooltip_opts=opts.TooltipOpts(trigger="axis"),
+    ).set_series_opts(
+        linestyle_opts=opts.LineStyleOpts(width=2),
+    )
+    # line.render_notebook()
+    return line
+
+
+# 暂时弃用
+# max_drawdown
+def get_max_drawdown(df_nav, column_name):
+    df_nav[f"{column_name}_max_so_far"] = df_nav[column_name].cummax()
+    df_nav[f"{column_name}_drawdown"] = (
+        df_nav[column_name] - df_nav[f"{column_name}_max_so_far"]
+    ) / df_nav[f"{column_name}_max_so_far"]
+    max_drawdown = df_nav[f"{column_name}_drawdown"].min()
+    return max_drawdown
