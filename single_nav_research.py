6af7c4f118911019d00bbd10fb90eb17f560a2f0
Slow-birds <1782023512@qq.com>
2025-01-21 17:02:31 +0800

    commit

--- single_nav_research.py
+++ single_nav_research.py
@@ -1,10 +1,10 @@
 from nav_research import NavResearch
 
-data_path = "资产净值公告_SVZ009_铭跃行远均衡一号私募证券投资基金_2022-07-29_2024-12-30_mul.xlsx"
-strategy = "量化CTA"
-fund_name = "铭跃行远均衡一号"
-benchmark_code = "NH0100.NHF"
-benchmark_name = "南华商品指数"
+data_path = "谊恒滴海一号净值20241231.xlsx"
+strategy = "ETF套利"
+fund_name = "谊恒滴海一号"
+benchmark_code = "H11009.CSI"
+benchmark_name = "中证综合债"
 threshold = -0.05
 
 demo = NavResearch(data_path,strategy,fund_name,benchmark_code,benchmark_name,threshold)
