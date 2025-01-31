dcf91eb656f5d1f3908360e11148ee37b888cafa
Slow-birds <1782023512@qq.com>
2025-01-31 11:46:58 +0800

    新建类

---
+++ huofuniu_data.py
@@ -0,0 +1,51 @@
+import requests
+import pandas as pd
+from pathlib import Path
+import time
+import os
+
+class NavData:
+    def __init__(self, fund_name, fund_coding):
+        self.fund_name = fund_name
+        self.fund_coding = fund_coding
+        self.token = "b80e7f84dec465d910073c99aea0c4ffbc64c7587cb005b5dfe9cbd767028d74dcff01e9e093d23385a27848395b0551"
+    def delete_csv_file(self):
+        for csv_file in Path("./data").glob(f"{self.fund_name}_*.csv"):
+            csv_file.unlink()
+            print(f"清理旧文件：{csv_file.name}")
+    def get_nav_data(self):
+        headers = {
+            "access-token": self.token,
+        }
+        url = f"https://pyapi.huofuniu.com/pyapi/fund/viewv2?fid={self.fund_coding}&pt=1&shareToken="
+        res = requests.get(url, headers=headers)
+        nav_data = pd.DataFrame(res.json()["data"]["fund"]["prices"])
+        nav_data = nav_data.rename(
+            columns={
+                "pd": "日期",
+                "nav": "单位净值",
+                "cnw": "累计净值",
+                "cn": "复权净值",
+                "pc": "涨跌幅",
+                "drawdown": "最大回撤",
+            }
+        ).drop(columns=["e_pc"])
+        nav_data = nav_data[["日期", "单位净值", "累计净值", "复权净值"]]
+        nav_data["日期"] = pd.to_datetime(nav_data["日期"])
+        path = Path(rf"data/{self.fund_name}_{nav_data["日期"].max().strftime("%Y-%m-%d")}.csv")
+        nav_data.to_csv(
+            path,
+            index=False,
+            encoding="utf-8-sig",
+        )
+        print(f"正在爬取{self.fund_name}净值，并生成新文件：{path}")
+        return nav_data
+
+if __name__ == "__main__":
+    fund_info = pd.read_csv('fund_list.txt', delimiter=' ')
+    for row in fund_info.itertuples(index=False, name=None):
+        demo = NavData(row[0], row[1])
+        demo.delete_csv_file()
+        demo.get_nav_data()
+        time.sleep(2)
+    print("所有产品净值爬取完成")
