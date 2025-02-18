51fd86d051b396401815d4a0c33e820b36dd3547
Slow-birds <1782023512@qq.com>
2025-02-18 17:19:50 +0800

    update

--- single_nav_research.py
+++ single_nav_research.py
@@ -1,10 +1,10 @@
 from nav_research import NavResearch
 
-data_path = "data\旌安1号_2025-02-07.csv"
-strategy = "套利"
-fund_name = "旌安1号"
-benchmark_code = "H11009.CSI"
-benchmark_name = "中证综合债"
+data_path = "简雍斯量先锋私募证券投资基金_净值序列_20250218.xlsx"
+strategy = "量化CTA"
+fund_name = "简雍斯量先锋"
+benchmark_code = "NH0100.NHF"
+benchmark_name = "南华商品指数"
 threshold = -0.05
 
 demo = NavResearch(data_path,strategy,fund_name,benchmark_code,benchmark_name,threshold)
